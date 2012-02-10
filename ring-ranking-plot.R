ba513a214cf0cff00ef2d79e54d7792251c56d5e
Luis A. Apiolaza <Luis.Apiolaza@canterbury.ac.nz>
2012-02-10 14:05:50 +1300

    Changed direction for graph

--- ring-ranking-plot.R
+++ ring-ranking-plot.R
@@ -6,30 +6,38 @@
 require(ggplot2)
 setwd('~/Documents/Research/2012/clones10')
 
+
+# Load Harewood ranking
+load('~/Documents/Research/2012/harewood/newmoe-genetic-values.Rdata')
+
 load('GeneticValues15Clones.Rdata')
+
+clone.thr <- merge(clone.thr, moe.gv[, c('TENclone', 'class')], by.x = 'clone', by.y = 'TENclone')
+clone.thr <- clone.thr[order(clone.thr$site, clone.thr$clone),]
+
 plot.d <- data.frame(clone = clone.thr$clone[1:15],
                      gvgold = clone.thr$effect[1:15],
-                     gvwait = clone.thr$effect[16:30])
+                     gvwait = clone.thr$effect[16:30],
+                     class = clone.thr$class[1:15])
 
 plot.d$gvmean <- with(plot.d, (gvgold + gvwait)/2)
 plot.d$gvmean <- plot.d$gvmean + 8
 
-plot.d$class <- factor(cut(plot.d$gvmean, breaks = c(0, 7.80, 8.15, 9), 
-                           labels = c('top', 'middle', 'bottom')))
-
 # Color-blind-friendly palette
 # http://wiki.stdout.org/rcookbook/Graphs/Colors%20(ggplot2)/
-cbfPalette <- scale_colour_manual(values=c("#56B4E9", "#000000", "#D55E00"))
+cbfPalette <- scale_colour_manual(values=c("#D55E00", "#000000", "#56B4E9"))
 
-ring.plot <- ggplot(plot.d, aes(x = gvmean, y = 1, colour = class, label = clone)) 
-ring.plot <- ring.plot + geom_text(size = 3, angle = 90) + 
-             scale_x_continuous('First ring to reach 7 GPa threshold') +
-             scale_y_continuous('') + cbfPalette +
+ring.plot <- ggplot(plot.d, aes(x = 1, y = gvmean, colour = class, label = clone)) 
+ring.plot <- ring.plot + geom_text(size = 3) + 
+             scale_y_reverse('First ring to reach 7 GPa threshold', angle = 90) +
+             scale_x_continuous('') + cbfPalette +
              opts(axis.title.x = theme_text(size = 12),
-             axis.text.x = theme_text(size = 10, colour = 'black'),
-             axis.text.y = theme_text(colour = 'white'),
+             axis.text.y = theme_text(size = 10, colour = 'black'),
+             axis.text.x = theme_text(colour = 'white'),
              legend.position = 'none')
 
-pdf('ring-ranking-plot.pdf', width = 7, height = 2)
+ring.plot
+
+pdf('ring-ranking-plot.pdf', width = 2, height = 7)
 ring.plot
 dev.off()
